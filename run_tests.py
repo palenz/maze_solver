c6aac569fb161edcf5098448518cdc7902f1db52
Juan <jpalenzuela@outlook.com>
2021-03-31 18:56:00 +0100

    initial setup

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.maze_test import TestMaze
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/maze_test.py
@@ -0,0 +1,10 @@
+import unittest
+from src.maze import *
+
+class TestMaze(unittest.TestCase):
+    
+    # def setUp(self):
+    #   self.a = 12
+
+    def load_data(self):
+        self.assertEqual(200, response.status_code)
